3e7b4e81de264b6657deb83283d926c611cf58d5
Lưu Bình An <luubinhan1989@gmail.com>
2019-03-30 10:25:25 +0700

    update

---
+++ .cache/async-requires.js
@@ -0,0 +1,17 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-service-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\templates\\service.js" /* webpackChunkName: "component---src-templates-service-js" */),
+  "component---src-templates-team-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\templates\\team.js" /* webpackChunkName: "component---src-templates-team-js" */),
+  "component---src-templates-testimonial-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\templates\\testimonial.js" /* webpackChunkName: "component---src-templates-testimonial-js" */),
+  "component---src-pages-404-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-contact-index-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\contact\\index.js" /* webpackChunkName: "component---src-pages-contact-index-js" */),
+  "component---src-pages-index-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-services-index-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\services\\index.js" /* webpackChunkName: "component---src-pages-services-index-js" */),
+  "component---src-pages-team-index-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\team\\index.js" /* webpackChunkName: "component---src-pages-team-index-js" */),
+  "component---src-pages-testimonials-index-js": () => import("C:\\xampp\\htdocs\\luubinhan.github.io\\src\\pages\\testimonials\\index.js" /* webpackChunkName: "component---src-pages-testimonials-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "C:\\xampp\\htdocs\\luubinhan.github.io\\.cache\\data.json")
+
