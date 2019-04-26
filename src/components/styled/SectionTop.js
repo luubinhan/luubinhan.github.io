2b7cc1a02a82fc15cc3dbef1a47ecc21a13a0ff4
Lưu Bình An <anlb@blockchainlabs.asia>
2019-04-26 17:05:04 +0700

    update animate

--- src/components/styled/SectionTop.js
+++ src/components/styled/SectionTop.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const SectionTop = styled.div`
   padding-bottom: 100px;
-  background-image: url("/images/avatar.jpg");
+  /* background-image: url("/images/avatar.jpg"); */
   background-repeat: no-repeat;
   background-position: 50vw 10vh;
   background-size: auto 60vh;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import Scroll from 'react-scroll';
 import Fade from 'react-reveal/Fade';
+import Reveal from 'react-reveal/Reveal';
 import SEO from '../components/SEO';
 import StyledBoxTitle from '../components/styled/StyledBoxTitle';
 import StyledWorkGroup from '../components/styled/StyledWorkGroup';
@@ -217,12 +218,20 @@ class Home extends React.Component {
 							<svg className="placeholder" width="528" height="396" viewBox="0 0 528 396">
 								<rect width="528" height="396" style={{fill: 'transparent'}}></rect>
 							</svg>
-							<div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
-							<div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
+              <Reveal effect="fadeInUp">
+							  <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>              
+              </Reveal>
+              <Reveal effect="animateBox2">
+							  <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
+              </Reveal>
 							<div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
 							<div className="hero-figure-box hero-figure-box-04" data-rotation="-135deg" ></div>
-							<div className="hero-figure-box hero-figure-box-05" ></div>
-							<div className="hero-figure-box hero-figure-box-06" ></div>
+              <Reveal effect="animateBox5">
+							  <div className="hero-figure-box hero-figure-box-05"></div>
+              </Reveal>
+              <Reveal effect="animateBox2">
+							  <div className="hero-figure-box hero-figure-box-06" ></div>
+              </Reveal>
 							<div className="hero-figure-box hero-figure-box-07" ></div>
 							<div className="hero-figure-box hero-figure-box-08" data-rotation="-22deg" ></div>
 							<div className="hero-figure-box hero-figure-box-09" data-rotation="-52deg" ></div>
