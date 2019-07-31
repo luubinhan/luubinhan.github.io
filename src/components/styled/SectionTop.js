ba4a9a1892d53469c420d6f6efddcb3b40093092
Lưu Bình An <anlb@blockchainlabs.asia>
2019-07-31 15:40:07 +0700

    style

--- src/components/styled/SectionTop.js
+++ src/components/styled/SectionTop.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const SectionTop = styled.div`
   padding-bottom: 100px;
   position: relative;
-  z-index: 2;
+  z-index: 999;
 `;
 
 export default SectionTop;
