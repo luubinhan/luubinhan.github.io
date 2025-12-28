381cdecb5b436f35493bd35cbd1734c980ade49c
anluu1 <anluu1@on.sinch.com>
2025-12-28 20:23:29 +0700

    refactor: simplify Footer component by removing props and StaticQuery

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { graphql, Link, StaticQuery } from 'gatsby';
 
-const Footer = props => (
+const Footer = () => (
   <div className="footer-strip">
     <div className="container">
       <div className="row">
         <div className="col-12">
           <div className="footer">
-            <h3 className="footer-title">{props.data.site.siteMetadata.title}</h3>
             <ul className="footer-menu">
               <li>
                 <a rel="noopener noreferrer" aria-label="Twitter Luu Binh An" target="_blank" href="https://twitter.com/luubinhan">
@@ -39,17 +37,4 @@ const Footer = props => (
   </div>
 );
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => <Footer data={data} />}
-  />
-);
+export default Footer;
