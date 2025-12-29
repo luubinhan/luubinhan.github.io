5cb80a8c488e23cb442fefecef35abd2353c29bc
anluu1 <anluu1@on.sinch.com>
2025-12-29 16:13:46 +0700

    refactor: remove debug log from WorkItem component

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './WorkItem.module.scss';
 
 const WorkItem = ({ column, color, children }) => {
-  console.log('WorkItem styles:', styles); // Debug log
   const columnClass = styles[`column${column}`] || '';
   const colorClass = color === '#C9DC08' ? styles.colorGreen : styles.colorBlue;
   
